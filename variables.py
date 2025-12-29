6fcc4a41480324c1e809cd28f45d792882f387da
RaghavSanghai <sanghairaghav@gmail.com>
2025-12-29 21:49:31 +0545

    Add examples of valid and invalid variable names

--- variables.py
+++ variables.py
@@ -13,3 +13,34 @@ print(p)
 age = 50
 print (Age) #'Age' not defined, case sensitive
 
+_age = 50
+print (_age)
+'''
+1st_name = "John"  # Invalid variable name, cannot start with a digit
+print (1st_name)
+first_name = "John"  # Valid variable name
+print (first_name)
+first-name = "John"  # Invalid variable name, cannot contain hyphen
+print (first-name)
+firstName = "John"  # Valid variable name
+print (firstName)
+first name = "John"  # Invalid variable name, cannot contain space
+print (first name)
+
+first_name$ = "John"  # Invalid variable name, cannot contain special character $
+print (first_name$)
+
+first_name_ = "John"  # Valid variable name
+print (first_name_)
+first$name = "John"  # Invalid variable name, cannot contain special character $
+print (first$name)
+# Reserved keywords cannot be used as variable names
+for = 10  # Invalid variable name, 'for' is a reserved keyword
+print (for)
+'''
+
+#print("hello world" using two different variables)
+a = "hello"
+b = "world"
+print (a, b) #or print(a + " " + b)
+
