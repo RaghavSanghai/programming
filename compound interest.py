b427f71708a32114623a8a2a4881905ce6781169
RaghavSanghai <sanghairaghav@gmail.com>
2026-01-10 15:29:30 +0545

    Added datatypes and keywords

--- compound interest.py
+++ compound interest.py
@@ -1,5 +1,5 @@
 p=float(input("Enter the principal amount: "))
 r=float(input("Enter the rate of interest: "))
 t=float(input("Enter the time (in years): "))
-ci=p*((1+r/100)**t)
+ci=(p*((1+r/100)**t))-p
 print("The compound interest is:",round(ci, 2))
