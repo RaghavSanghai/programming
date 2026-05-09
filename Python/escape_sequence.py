08a1a67a273dbfe965d6e912fa54af0fafb15cdf
RaghavSanghai <sanghairaghav@gmail.com>
2026-05-09 16:32:53 +0545

    Added

---
+++ Python/escape_sequence.py
@@ -0,0 +1,11 @@
+# \n - Newline
+# \t - Tab
+# \\ - Backslash
+# \' - Single Quote
+# \" - Double Quote
+# Example usage of escape sequences
+print("Hello, World!\nWelcome to Python programming.")
+print("This is a tab:\tSee the space after the tab?")
+print("This is a backslash: \\")
+print("She said, \"Hello!\"")
+print('It\'s a nice day!')
