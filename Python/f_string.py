4406d12222de59805547acfa8b278eb33c491bc4
RaghavSanghai <sanghairaghav@gmail.com>
2026-05-09 13:30:35 +0545

    Added

---
+++ Python/f_string.py
@@ -0,0 +1,11 @@
+name="John"
+age=20
+language="Python"
+hours=3
+
+# John is 20 years old. He is learning Python for 3 hours a day.
+
+print(name, "is", age, "years old. He is learning", language, "for", hours, "hours a day.") # Default separator is space
+
+# Using f-string for better readability
+print(f"{name} is {age} years old. He is learning {language} for {hours} hours a day.") # Using f-string for better readability
