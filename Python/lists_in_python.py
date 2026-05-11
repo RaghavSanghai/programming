5ef83286d2d7ed7218a2aa64d81d89eb9df0c9a6
RaghavSanghai <sanghairaghav@gmail.com>
2026-05-11 10:36:20 +0545

    add

---
+++ Python/lists_in_python.py
@@ -0,0 +1,11 @@
+name = "John"
+age = 20
+percentage = 85.5
+
+student = ["John", 20, 85.5]
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+print(days[0])  # Output: Mon
+print(days[5])  # Output: Sat
+print(f"Last day of the week is: {days[-1]}")  # Output: Sun
+
