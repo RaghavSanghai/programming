11247e4bd368897549c3707bf9e4ae92fd03dee2
RaghavSanghai <sanghairaghav@gmail.com>
2026-05-11 22:00:58 +0545

    add

---
+++ Python/nested_list.py
@@ -0,0 +1,11 @@
+l1=[5,1,5,"Python",True,None,[1,2,3],10]
+print(len(l1))  # Output: 7
+print(l1[0])    # Output: 5
+print(l1[3])    # Output: "Python"
+print(l1[-1])   # Output: [1, 2, 3]
+print(l1[-2][1])  # Output: 2
+
+l2=[[-1,2],[3,4],[5,6,[7,8]]]
+print(l2[0])      # Output: [-1, 2]
+print(l2[2][2])   # Output: [7, 8]
+print(l2[2][2][0])  # Output: 7
